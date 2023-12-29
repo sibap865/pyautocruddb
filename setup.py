9c577cb5f96678ff28b1ee19b649ded9cd653ed0
sibaprasad <sibap865@gmail.com>
2023-12-29 21:49:30 +0530

    temlate updated

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+__version__ = "0.0.1"
+REPO_NAME = "pyautocruddb"
+PKG_NAME= "pyautocruddb"
+AUTHOR_USER_NAME = "sibap865"
+AUTHOR_EMAIL = "sibap865@gmail.com"
 
 setup(
     name=PKG_NAME,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A python package for automatic crud operation on mogodb, cassandra and mysql databases.",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
